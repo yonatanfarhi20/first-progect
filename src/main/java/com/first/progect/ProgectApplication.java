31b981a87f10287f3301848120d1fc42c4de3724
Yonatan Farhi <yonatanfarhi1@gmail.com>
2025-11-03 17:28:53 +0200

    first commit

---
+++ src/main/java/com/first/progect/ProgectApplication.java
@@ -0,0 +1,13 @@
+package com.first.progect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/progect/ProgectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.progect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
